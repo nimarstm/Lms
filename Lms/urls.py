3a31150debf6f039dcf74b0cf48d8479e12dcd42
Nima <nimars1394sh@gmail.com>
2024-09-20 08:24:09 -0500

    swagger added

--- Lms/urls.py
+++ Lms/urls.py
@@ -1,25 +1,24 @@
-"""
-URL configuration for Lms project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls.py import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
-"""
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from .local_settings import *
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library API",
+      default_version='v1',
+      description="API documentation for the library project",
+      terms_of_service="https://www.example.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
@@ -28,6 +27,9 @@ urlpatterns = [
     path('', include('rating_and_review.urls')),
     path('', include('notifications.urls')),
     path('reports/', include('reports.urls')),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 if IS_DEVEL:
